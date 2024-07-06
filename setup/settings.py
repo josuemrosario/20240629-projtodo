1147186afe83bc339d41e5cb312ff039892db07b
Josue Rosario <josue.rosario@escolar509.sp.senai.br>
2024-07-06 17:13:49 -0300

    aula 06 07 2024 - template listagem tarefas

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n4u#up1o^jsc&-&$*g2lbiy3ru+%9kid*o=d377@$f427vs3xo'
+#SECRET_KEY = 'django-insecure-n4u#up1o^jsc&-&$*g2lbiy3ru+%9kid*o=d377@$f427vs3xo'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS",cast=Csv())
 
 
 # Application definition
